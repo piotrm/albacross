3d28659ab3c87cf4cf0da54b3970ac40a0a5f717
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2017-10-26 05:14:19 +0200

    Does some cleaning in Gemfile and config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,5 @@ module Albacross
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.autoload_paths << "#{Rails.root}/lib"
-    config.autoload_paths << "#{Rails.root}/app/sanitizers"
   end
 end
